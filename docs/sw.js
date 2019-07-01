f48f7cd85071c4a7d93e0fe5e00f45ea1f0a2efa
Ash <38077119+jollyash@users.noreply.github.com>
2019-07-01 15:16:26 +0100

    added styling to ios prompt

--- docs/sw.js
+++ docs/sw.js
@@ -1,4 +1,4 @@
-importScripts("precache-manifest.7d15f589ddd52e6b2262bd5230472bff.js", "https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+importScripts("precache-manifest.b381d34d42ec931caa2ba4d4d0931a69.js", "https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 
 // Watch for install
